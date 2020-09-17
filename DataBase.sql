4fd54178e2b0316a07d94a084ede74e71d0388ac
Nunna Satya Surya Venkat <31370698+satyasuryavenkat@users.noreply.github.com>
2020-09-18 02:43:22 +0530

    Update DataBase.sql

--- DataBase.sql
+++ DataBase.sql
@@ -1,1 +1,11 @@
 create table expense(id int not null auto_increment,expensetype varchar(100),expenseitem varchar(100),expensemoney int,expensedate varchar(100), primary key(id));
+
+create table expensetype(expensetype varchar(100));
+                                             
+insert into expensetype values("Food");
+insert into expensetype values("Travel");
+insert into expensetype values("Shopping");
+insert into expensetype values("Rent");
+insert into expensetype values("Bills");
+insert into expensetype values("Medical");
+insert into expensetype values("Groceries");                                             
